ead3762cbc5797edfe4cbb4a78d317da1f99e4f8
John Brunton <john_brunton@live.co.uk>
2020-08-08 15:58:15 +0100

    add kotlin

---
+++ android/app/src/main/java/com/reacttest/MyReactActivity.kt
@@ -0,0 +1,13 @@
+package com.reacttest
+
+import com.facebook.react.ReactActivity
+
+class MyReactActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String? {
+        return "ReactTest"
+    }
+}
